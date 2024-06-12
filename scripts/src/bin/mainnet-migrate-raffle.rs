91bf74f040fc6972e5fe30d8694d191799e8fd48
Kayanski <kowalski.kowalskin@gmail.com>
2024-06-12 07:51:15 +0000

    Nit

--- scripts/src/bin/mainnet-migrate-raffle.rs
+++ scripts/src/bin/mainnet-migrate-raffle.rs
@@ -38,26 +38,5 @@ pub fn main() -> anyhow::Result<()> {
         vote: None,
     })?;
 
-    // raffles.instantiate(
-    //     &InstantiateMsg {
-    //         name: "Raffle Contract".to_string(),
-    //         nois_proxy_addr: "stars1atcndw8yfrulzux6vg6wtw2c0u4y5wvy9423255h472f4x3gn8dq0v8j45"
-    //             .to_string(),
-    //         nois_proxy_coin: coin(
-    //             1_000_000,
-    //             "ibc/ACCAF790E082E772691A20B0208FB972AD3A01C2DE0D7E8C479CCABF6C9F39B1",
-    //         ),
-    //         owner: None,
-    //         fee_addr: Some(chain.sender().to_string()),
-    //         minimum_raffle_duration: Some(60),
-    //         max_ticket_number: None,
-    //         raffle_fee: Decimal::percent(10),
-    //         creation_coins: Some(coins(45, "ustars")),
-    //         fee_discounts: vec![],
-    //     },
-    //     None,
-    //     None,
-    // )?;
-
     Ok(())
 }
